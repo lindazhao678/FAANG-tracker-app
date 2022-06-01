be9fe3b77d818991a1435d3d35267da176ad12fc
lindazhao678 <lindazhao678@gmail.com>
2022-06-01 11:52:18 +1000

    server name adjust

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const SERVER_NAME = dev ? 'http://localhost:3000' : 'https://faangtrackerapp.vercel.app'
+export const SERVER_NAME = dev ? 'http://localhost:3000' : 'https://faang-tracker-app.vercel.app'
