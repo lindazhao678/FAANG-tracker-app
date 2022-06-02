03d12ba8e2dc08115c0fcd15fccfa5300605c941
lindazhao678 <lindazhao678@gmail.com>
2022-06-02 15:31:59 +1000

    googleAnalytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,34 @@
 import '../styles/sass/globals.scss'
 import Layout from '../components/layout/Layout'
+import Script from 'next/script'
+import { Fragment } from 'react'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Fragment>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+        strategy='afterInteractive'
+      />
+
+      <Script
+        id="google-analytics-script"
+        strategy="afterInteractive"
+      >
+        {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+        
+      gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
+    `}
+      </Script>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+
+    </Fragment>
   )
 }
 
